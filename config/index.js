159670ede77fb26980e7e8ceddfd91e45895dc96
Guo213sas1 <111879704+Guo213sas1@users.noreply.github.com>
2023-11-07 20:30:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx8b03305d231f0f46",
+  appsecret: "f816387b7fc05adb88ad112c199c5e7d",
+  template_id: "WNJ4pfcfO--CDL_-737TS6B9tgLFliJTjDCPHbuFrrA",
+  touser: "otsCv6RYTRBjLp8dtX9uXfjNJwFo",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "太原",
+  first_date: "2023-10-28",
 };
